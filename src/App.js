a2d874038b0a40cff348fe52728999ad65b2778b
moossttafa <mostafo7102000@gmail.com>
2022-04-03 19:20:49 +0300

    edit ui component

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home/Home';  
 import Footer from "./components/Footer/Footer"
-// import NotFound from "./Pages/NotFound/NotFound"
+import NotFound from "./Pages/NotFound/NotFound"
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header /> 
+      <Header /> 
+      <Switch> 
          <Route path="/" exact component={Home} /> 
           <Route path="/about" component={About} />
           <Route path="/product/:productId" component={ProductDetails} />
           <Route path="/product" component={ProductListing} />
           <Route path="/contact" component={Contact} />
-          {
-          // <Route path="*" component={NotFound} />  
-          }
+          <Route path="*">
+            <NotFound />
+            </Route>
+            </Switch>
           <Footer />
         </Router>  
     </div>
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./style.css"
 
 const NotFound = () => {
   return (
-    <h1>NotFound</h1>
+    <section className="not-found">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="back">back home</Link>
+      </section>
+    </section>
   )
 }
 
